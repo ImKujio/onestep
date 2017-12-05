99e2dbc17b1ae4c663ac0dc00b5253e3e587befb
dxys <291460573@qq.com>
2017-12-05 21:55:10 +0800

    添加注释

--- app/src/main/java/com/dxys/onestep/weixins/MainActivity.java
+++ app/src/main/java/com/dxys/onestep/weixins/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dxys.onestep.weixins;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -11,6 +12,12 @@ import java.io.InputStreamReader;
 
 public class MainActivity extends Activity {
 
+
+    /** method
+     * Des: 启动微信付款码扫一扫，su用户执行am命令实现，此方法可以启动任意App的任意activity
+     * Param:
+     * Return:
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,7 @@ public class MainActivity extends Activity {
                 try {
                     Process process = Runtime.getRuntime().exec("su");
                     DataOutputStream os = new DataOutputStream(process.getOutputStream());
+//                    执行Shell的am命令具体可以自行谷歌百度
                     os.write("am start com.tencent.mm/.plugin.scanner.ui.BaseScanUI\nexit\n".getBytes());
                     os.close();
                     if (process.waitFor() == 0)
@@ -96,4 +104,25 @@ public class MainActivity extends Activity {
         }).start();
         moveTaskToBack(false);
     }
+
+    /** class
+     * Des: 启动支付宝付款码、扫一扫，通过支付宝的启动Intent过滤器实现
+     * 阿里付款码uri：alipayqr://platformapi/startapp?saId=20000056
+     * 阿里扫一扫uri:alipayqr://platformapi/startapp?saId=10000007
+     */
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        Toast.makeText(this, "正在启动支付宝付款码...", Toast.LENGTH_SHORT).show();
+//        try{
+//            Intent intent = Intent.parseUri("alipayqr://platformapi/startapp?saId=20000056",Intent.URI_INTENT_SCHEME);
+//            intent.addCategory(Intent.CATEGORY_BROWSABLE);
+//            startActivity(intent);
+//        }catch (Exception e)
+//        {
+//            Toast.makeText(this, "启动失败！", Toast.LENGTH_SHORT).show();
+//        }
+//        finish();
+//    }
+
 }
